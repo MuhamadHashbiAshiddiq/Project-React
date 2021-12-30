106ec0c3baca8ce45669969ad11213b78f42ed24
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-31 00:57:09 +0700

    e: Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -8,7 +8,9 @@ export default class Register extends Component {
   password = "";
   password_confirm = "";
 
-  submit = (w: SyntheticEvent) => {
+  submit = (e: SyntheticEvent) => {
+    e.preventDefault();
+
     console.log({
       first_name: this.first_name,
       last_name: this.last_name,
@@ -23,16 +25,12 @@ export default class Register extends Component {
       <main className="form-signin">
         <form onSubmit={this.submit}>
           <h1 className="h3 mb-3 fw-normal">Please register</h1>
-
           <input type="First Name" className="form-control" placeholder="First Name" required onChange={(e) => (this.first_name = e.target.value)} />
-
           <input type="Last Name" className="form-control" placeholder="Last Name" required onChange={(e) => (this.last_name = e.target.value)} />
-
           <input type="email" className="form-control" placeholder="Email" required onChange={(e) => (this.email = e.target.value)} />
-
           <input type="password" className="form-control" placeholder="Password" required onChange={(e) => (this.password = e.target.value)} />
-
-          <input type="password" className="form-control" placeholder="Password Confirm" required onChange={(e) => (this.password_confirm = e.target.value)} />
+          <input type="password" className="form-control" placeholder="Password Confirm" required onChange={(e) => (this.password_confirm = e.target.value)
+          } />
 
           <button className="w-100 btn btn-lg btn-primary" type="submit">
             Submit
