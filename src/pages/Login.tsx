9bb48ace5fec2369682af8f39e16abf95972786f
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-31 14:28:31 +0700

    a: useState

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      Login
+    <div className="container">
+      <h1>Count {count}</h1>
+      <input type="number" onChange={e => setCount(parseInt(e.target.value))}/>
     </div>
   )
 }
