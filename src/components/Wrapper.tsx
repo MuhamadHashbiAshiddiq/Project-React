884cc3515cc07507c176f5ea3b1162edbf5e880c
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-31 17:19:01 +0700

    a: Logout

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,21 +1,39 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Menu from "./Menu";
 import Nav from "./Nav";
+import { Navigate } from "react-router-dom";
 
-export default class Wrapper extends Component {
-  render() {
-    return (
-      <>
-        <Nav />
+const Wrapper = (props: any) => {
+  const [navigate, setNavigate] = useState(false);
 
-        <div className="container-fluid">
-          <div className="row">
-            <Menu />
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("user");
+      } catch (e) {
+        setNavigate(true);
+      }
+    })();
+  }, []);
 
-            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">{this.props.children}</main>
-          </div>
-        </div>
-      </>
-    );
+  if (navigate) {
+    return <Navigate to="login" />;
   }
-}
+
+  return (
+    <>
+      <Nav />
+
+      <div className="container-fluid">
+        <div className="row">
+          <Menu />
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">{props.children}</main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Wrapper;
