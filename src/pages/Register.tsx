0d953fef970335699f89638775c0b1bd0efd23fa
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-31 14:16:57 +0700

    a: State (bt)

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import React, { Component, SyntheticEvent } from "react";
 import "../Login.css";
-import axios from 'axios';
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 export default class Register extends Component {
   first_name = "";
@@ -8,21 +9,28 @@ export default class Register extends Component {
   email = "";
   password = "";
   password_confirm = "";
+  state = {
+    redirect: false,
+  };
 
   submit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
-    const response = await axios.post("http://localhost:8000/api/register", {
+    await axios.post("http://localhost:8000/api/register", {
       first_name: this.first_name,
       last_name: this.last_name,
       email: this.email,
       password: this.password,
       password_confirm: this.password_confirm,
     });
-    console.log(response);
+    this.setState({ redirect: true });
   };
 
   render() {
+    if (this.state.redirect) {
+      return <Navigate to={"login"} />;
+    }
+
     return (
       <main className="form-signin">
         <form onSubmit={this.submit}>
