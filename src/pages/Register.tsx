ab5db937ed1b821ff9a8feecada963c401344e95
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2021-12-31 13:33:47 +0700

    a: Http Requests

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import React, { Component, SyntheticEvent } from "react";
 import "../Login.css";
+import axios from 'axios';
 
 export default class Register extends Component {
   first_name = "";
@@ -8,16 +9,17 @@ export default class Register extends Component {
   password = "";
   password_confirm = "";
 
-  submit = (e: SyntheticEvent) => {
+  submit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
-    console.log({
+    const response = await axios.post("http://localhost:8000/api/register", {
       first_name: this.first_name,
       last_name: this.last_name,
       email: this.email,
       password: this.password,
       password_confirm: this.password_confirm,
     });
+    console.log(response);
   };
 
   render() {
@@ -29,8 +31,7 @@ export default class Register extends Component {
           <input type="Last Name" className="form-control" placeholder="Last Name" required onChange={(e) => (this.last_name = e.target.value)} />
           <input type="email" className="form-control" placeholder="Email" required onChange={(e) => (this.email = e.target.value)} />
           <input type="password" className="form-control" placeholder="Password" required onChange={(e) => (this.password = e.target.value)} />
-          <input type="password" className="form-control" placeholder="Password Confirm" required onChange={(e) => (this.password_confirm = e.target.value)
-          } />
+          <input type="password" className="form-control" placeholder="Password Confirm" required onChange={(e) => (this.password_confirm = e.target.value)} />
 
           <button className="w-100 btn btn-lg btn-primary" type="submit">
             Submit
