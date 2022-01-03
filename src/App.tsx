3c1f41e53a8014c20e31e946a960e732ad262519
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-03 13:33:31 +0700

    a: Roles (bt)

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Register from "./pages/Register";
 import UserCreate from "./pages/users/UserCreate";
 import UserEdit from "./pages/users/UserEdit";
 import Users from "./pages/users/Users";
+import Roles from './pages/roles/Roles';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path={"login/*"} element={<Login />} />
           <Route path={"users/create/*"} element={<UserCreate />} />
           <Route path={"users/:id/edit*"} element={<UserEdit />} />
+          <Route path={"roles*"} element={<Roles />} />
         </Routes>
       </BrowserRouter>
     </div>
